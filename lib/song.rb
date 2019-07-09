09950abe5a77e6a2c69f23ed4112f8764323a941
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-09 19:55:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,8 +43,14 @@ attr_accessor :name, :artist, :genre
   end
 
   def self.genre_count
-    @@genres.each {|genre| binding.pry}
-
+    @@genres.each do |genre|
+      if @@genre_count[genre]
+        @@genre_count[genre] += 1
+      else
+        @@genre_count[genre] = 1
+      end
+    end
+    @@genre_count     
   end
   # def self.artist_count
   #
